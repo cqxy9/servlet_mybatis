95d7fcddac18fc7995165ee605f182ac43ffa586
XY <123@163.com>
2020-03-13 23:49:51 +0800

    维护

--- src/main/java/com/xy/dao/GetSqlSession.java
+++ src/main/java/com/xy/dao/GetSqlSession.java
@@ -5,54 +5,53 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
-  * Copyright 软件技术有限公司
-  * All rights reserved
-  *
-  * @Description:
-  *
-  * @Author: XY
-  * @Date: 2020/2/26
-  */
+ * Copyright 软件技术有限公司
+ * All rights reserved
+ *
+ * @Description:数据交互
+ * @Author: XY
+ * @Date: 2020/2/26
+ */
 public class GetSqlSession {
     private static final Logger log = LoggerFactory.getLogger(GetSqlSession.class);
 
-    private static ThreadLocal<SqlSession> tl = new ThreadLocal<>();
+    private static ThreadLocal<SqlSession> threadLocal = new ThreadLocal<>();
 
     public static SqlSession getSqlSession() throws Exception {
-        SqlSession sqlSession = tl.get();
+        SqlSession sqlSession = threadLocal.get();
         if (null == sqlSession) {
             sqlSession = GetSqlSessionFactory.getInstance().getSqlSessionFactory().openSession();
-            tl.set(sqlSession);
+            threadLocal.set(sqlSession);
         }
         log.info("数据session hashCode : {}.", sqlSession.hashCode());
         return sqlSession;
     }
 
     public static void commit() {
-        SqlSession sqlSession = tl.get();
+        SqlSession sqlSession = threadLocal.get();
         if (null != sqlSession) {
             sqlSession.commit();
             sqlSession.close();
-            tl.set(null);
+            threadLocal.remove();
             log.info("数据commit");
         }
     }
 
     public static void rollback() {
-        SqlSession sqlSession = tl.get();
+        SqlSession sqlSession = threadLocal.get();
         if (null != sqlSession) {
             sqlSession.rollback();
             sqlSession.close();
-            tl.set(null);
+            threadLocal.remove();
             log.info("数据rollback");
         }
     }
 
     public static void queryClose() {
-        SqlSession sqlSession = tl.get();
+        SqlSession sqlSession = threadLocal.get();
         if (null != sqlSession) {
             sqlSession.close();
-            tl.set(null);
+            threadLocal.remove();
             log.info("获取数据流close");
         }
     }
